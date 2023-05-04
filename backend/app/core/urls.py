0906962fee78f7b6940d5ca006948c74ad3cbd29
DarkLorianPrime <lolpokens@mail.ru>
2023-05-05 02:26:39 +0400

    Connect clear swagger + openapi

--- backend/app/core/urls.py
+++ backend/app/core/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularSwaggerView, SpectacularAPIView, SpectacularRedocView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("authserver.urls")),
-    path('friends/', include("friends.urls"))
+    path('friends/', include("friends.urls")),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    # Optional UI:
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+
 ]
