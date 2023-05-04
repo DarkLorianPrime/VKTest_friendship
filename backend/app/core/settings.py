1d18bd91f21e76882a9bec994aeecfe25c96c0a3
DarkLorianPrime <lolpokens@mail.ru>
2023-05-05 00:30:07 +0400

    off debug mode

--- backend/app/core/settings.py
+++ backend/app/core/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
