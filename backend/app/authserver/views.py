bd1bb8d318ad6e93c774f67a3553a1dc173764ba
DarkLorianPrime <lolpokens@mail.ru>
2023-05-05 23:57:06 +0400

    Add openapi specs

--- backend/app/authserver/views.py
+++ backend/app/authserver/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from drf_spectacular.utils import extend_schema, OpenApiParameter
 from rest_framework.permissions import AllowAny
 from rest_framework.request import Request
 from rest_framework.response import Response
@@ -13,6 +14,9 @@ class Authorization(ModelViewSet):
     permission_classes: list = [AllowAny]
     serializer_class: Serializer = UserSerializer
 
+    @extend_schema(
+        description="Регистрирует пользователя с переданным username и паролем.",
+    )
     def create(self, request: Request, *args, **kwargs):
         serialized: ModelSerializer = self.get_serializer(data=request.POST)
         serialized.is_valid(raise_exception=True)
